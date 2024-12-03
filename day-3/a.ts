87d110029d9368797a26d0bfcb438d0af8ccd4e9
Floris <florisjoepie@gmail.com>
2024-12-03 21:26:37 +0100

    feat: aoc day 3

--- day-3/a.ts
+++ day-3/a.ts
@@ -2,8 +2,13 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0
+  const test = data.toString().matchAll(/mul\(\d+,\d+\)/g)
+  test.forEach((val) => {
+    const numbers = val[0].match(/\d+/g)
+    sum += parseInt(numbers[0]) * parseInt(numbers[1])
+  })
+  return sum;
 }
 
 await runSolution(day3a);
--- day-3/b.ts
+++ day-3/b.ts
@@ -2,8 +2,20 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0
+  const test = data.toString().matchAll(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g)
+  let shouldMultiply = true
+  test.forEach((val) => {
+    if (val[0] === 'do()'){
+      shouldMultiply = true
+    } else if (val[0] === "don't()"){
+      shouldMultiply = false
+    } else if (shouldMultiply){
+      const numbers = val[0].match(/\d+/g)
+      sum += parseInt(numbers[0]) * parseInt(numbers[1])
+    }
+  })
+  return sum;
 }
 
 await runSolution(day3b);
