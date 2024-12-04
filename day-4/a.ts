beac617d791848796c8df1a704ea43433130cd4b
Floris <florisjoepie@gmail.com>
2024-12-04 08:27:55 +0100

    feat: aoc day 4

--- day-4/a.ts
+++ day-4/a.ts
@@ -1,9 +1,104 @@
 import { runSolution } from '../utils.ts';
 
+const findXMAXorXMASReverse = (val: string) => {
+  const xmas = val.match(/XMAS/g)
+  const xmasReverse = val.match(/SAMX/g)
+
+  return {xmas, xmasReverse}
+}
+
+const diagonal = (data: string[]) => {
+  let sum = 0
+    //find diagonal
+
+  //based vertically
+  data.forEach((row, index) => {
+    const verticalRowArray = []
+    let startPos = 0
+    for(let y = index; y < data.length; y++){
+        const value = data[y].charAt(startPos)
+        if (value){
+          verticalRowArray.push(value)
+          startPos += 1
+        }
+    }
+    const rowAsString = verticalRowArray.join('')
+    const {xmasReverse, xmas} = findXMAXorXMASReverse(rowAsString)
+    if (xmas) {
+      sum += xmas.length
+    }
+    if(xmasReverse){
+      sum += xmasReverse.length
+    }
+  })
+
+  // based horizontally
+  for(let i = 1; i < data[0].length; i++) {
+    const horizontalBasedVerticalArray = []
+    let startPos = i
+    for(let y = 0; y < data.length; y++){
+      const value = data[y].charAt(startPos)
+      if (value){
+        horizontalBasedVerticalArray.push(value)
+        startPos += 1
+      }
+     
+    }
+    // console.log(horizontalBasedVerticalArray, 'horizontal')
+
+    const rowAsString = horizontalBasedVerticalArray.join('')
+    const {xmasReverse, xmas} = findXMAXorXMASReverse(rowAsString)
+    if (xmas) {
+      sum += xmas.length
+    }
+    if(xmasReverse){
+      sum += xmasReverse.length
+    }
+  }
+
+  return sum;
+}
+
 /** provide your solution as the return of this function */
 export async function day4a(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0
+
+  //find horizontal
+  data.forEach(val => {
+    const {xmasReverse, xmas} = findXMAXorXMASReverse(val)
+    if (xmas) {
+      sum += xmas.length
+    }
+    if(xmasReverse){
+      sum += xmasReverse.length
+    }
+  })
+
+  //find vertical
+  for(let i = 0; i < data[0].length; i++){
+    const rowArray = []
+    data.forEach(row => {
+      rowArray.push(row[i])
+    })
+    const rowAsString = rowArray.join('')
+    const {xmasReverse, xmas} = findXMAXorXMASReverse(rowAsString)
+    if (xmas) {
+      sum += xmas.length
+    }
+    if(xmasReverse){
+      sum += xmasReverse.length
+    }
+  }
+
+  const normalDiagonal = diagonal(data)
+  const dataReverse = []
+  data.forEach(row => {
+    dataReverse.push(row.split('').reverse().join('').replace('\r', ''))
+  })
+ const reverseDiagonal = diagonal(dataReverse)
+
+  return sum + normalDiagonal + reverseDiagonal;
 }
 
 await runSolution(day4a);
+
--- day-4/b.ts
+++ day-4/b.ts
@@ -2,8 +2,44 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day4b(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0;
+  for(let x = 0; x < data[0].length; x++){
+    if (x === 0 || x === data[0].length) {
+      continue
+    }
+    for(let y = 0; y < data.length; y++){
+      if (y === 0 || y === data.length) {
+        continue
+      }
+      const value = data[y].charAt(x)
+      if (value !== 'A') {
+        continue
+      }
+      const diagonal1 = []
+      const diagonal2 = []
+
+      diagonal1.push(data[y - 1]?.charAt(x - 1))
+      diagonal1.push('A')
+      diagonal1.push(data[y + 1]?.charAt(x + 1))
+
+      diagonal2.push(data[y - 1]?.charAt(x + 1))
+      diagonal2.push('A')
+      diagonal2.push(data[y + 1]?.charAt(x - 1))
+      
+      const val1 = diagonal1.join('')
+      const val2 = diagonal2.join('')
+
+      if (val1 !== "MAS" && val1 !== "SAM") {
+        continue
+      }
+      if (val2 !== "MAS" && val2 !== "SAM") {
+        continue
+      }
+
+      sum += 1
+    }
+  }
+  return sum;
 }
 
 await runSolution(day4b);
