ffcaff680a99321a06a5da7538aa8d980b6ccc5a
Floris Jetses <floris.jetses@deptagency.com>
2024-12-05 15:32:28 +0100

    feat: day 2

--- day-2/a.ts
+++ day-2/a.ts
@@ -2,8 +2,23 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0
+  data.forEach(item => {
+    let values = item.split(' ')
+    if (!values[0]) return
+    if (Number(values[1]) <= Number(values[0])){
+      values = values.reverse()
+    }
+    const isUnsafe = values.some((val, index) => {
+      if (index === 0) return false
+      const previous = values[index - 1]
+      return !(((val - previous) > 0) && Math.abs(previous - val) <= 3)
+    })
+    if (!isUnsafe){
+      sum += 1
+    }
+  })
+  return sum;
 }
 
 await runSolution(day2a);
--- day-2/b.ts
+++ day-2/b.ts
@@ -1,9 +1,41 @@
 import { runSolution } from '../utils.ts';
 
+const safetyInspector = (arr: string[]) => {
+  const isUnsafe = arr.some((val, index) => {
+    if (index === 0) return false
+    const previous = arr[index - 1]
+    return !(((val - previous) > 0) && Math.abs(previous - val) <= 3)
+  })
+  return isUnsafe
+}
+
 /** provide your solution as the return of this function */
 export async function day2b(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0
+  data.forEach(item => {
+    let values = item.split(' ')
+    if (!values[0]) return
+    if (Number(values[values.length - 1]) <= Number(values[0])){
+      values = values.reverse()
+    }
+    const isUnsafe = safetyInspector(values)
+    if (!isUnsafe){
+      sum += 1
+    } else {
+      let isSafe = false
+      values.forEach((val, index) => {
+        if (isSafe) return
+        const newValues = [...values]
+        newValues.splice(index, 1)
+        const newIsUnsafe = safetyInspector(newValues)
+        if (!newIsUnsafe){
+          sum += 1
+          isSafe = true
+        }
+      })
+    }
+  })
+  return sum;
 }
 
 await runSolution(day2b);
