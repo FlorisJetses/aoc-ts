a514a65453f1724db96e2cd231d736acb5bc753d
Floris Jetses <floris.jetses@deptagency.com>
2024-12-08 14:03:07 +0100

    feat: day 8

--- day-8/a.ts
+++ day-8/a.ts
@@ -2,8 +2,63 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day8a(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0;
+  const map = data.map(row => {
+    if (row.length){
+     return row.replace('\r', '').split('')
+    }
+  }).filter(Boolean)
+  const mapLength = map.length
+  const mapWidth = map[0].length
+
+  const frequencies: Record<string | number, {locations: {x: number; y: number}[]}> = {}
+  for(let y = 0; y < mapLength; y++){
+    for (let x = 0; x < mapWidth; x++){
+      const value = map[y][x]
+
+      if (!value) continue
+
+      const match = value.match(/[0-9a-zA-Z]/g)
+      if(match){
+        if (!frequencies[value]){
+          frequencies[value] = {locations: [{x, y}]}
+        } else {
+          frequencies[value].locations.push({x, y})
+        }
+      }
+    }
+  }
+
+  const uniqueLocs: Record<number, Record<number, boolean>> = {}
+
+  Object.values(frequencies).forEach(freq => {
+    freq.locations.forEach((loc, index) => {
+      for(let i = 0; i < freq.locations.length - (index + 1); i++){
+        const location = freq.locations[i + 1 + index]
+        const yDiff = Math.abs(loc.y - location.y)
+        const xDiff = Math.abs(loc.x - location.x)
+
+        const point1 = {x: loc.x < location.x ? loc.x - xDiff : loc.x + xDiff, y: loc.y - yDiff}
+        const point2 = {x: loc.x < location.x ? location.x + xDiff : location.x - xDiff, y: location.y + yDiff}
+        if (!uniqueLocs[point1.y]?.[point1.x]){
+          if (point1.x < mapWidth && point1.x >= 0 && point1.y < mapLength && point1.y >= 0){
+            uniqueLocs[point1.y] = {...uniqueLocs[point1.y], [point1.x]: true}
+          }
+        }
+
+        if (!uniqueLocs[point2.y]?.[point2.x]){
+          if (point2.x < mapWidth && point2.x >= 0 && point2.y < mapLength && point2.y >= 0){
+            uniqueLocs[point2.y] = {...uniqueLocs[point2.y], [point2.x]: true}
+          }
+        }
+
+      }
+    })
+  })
+  Object.values(uniqueLocs).forEach(loc => {
+    sum += Object.keys(loc).length
+  })
+  return sum;
 }
 
 await runSolution(day8a);
--- day-8/b.ts
+++ day-8/b.ts
@@ -2,8 +2,73 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day8b(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0;
+  const map = data.map(row => {
+    if (row.length){
+     return row.replace('\r', '').split('')
+    }
+  }).filter(Boolean)
+  const mapLength = map.length
+  const mapWidth = map[0].length
+
+  const frequencies: Record<string | number, {locations: {x: number; y: number}[]}> = {}
+  for(let y = 0; y < mapLength; y++){
+    for (let x = 0; x < mapWidth; x++){
+      const value = map[y][x]
+
+      if (!value) continue
+
+      const match = value.match(/[0-9a-zA-Z]/g)
+      if(match){
+        if (!frequencies[value]){
+          frequencies[value] = {locations: [{x, y}]}
+        } else {
+          frequencies[value].locations.push({x, y})
+        }
+      }
+    }
+  }
+
+  const uniqueLocs: Record<number, Record<number, boolean>> = {}
+
+  Object.values(frequencies).forEach(freq => {
+    freq.locations.forEach((loc, index) => {
+      for(let i = 0; i < freq.locations.length - (index + 1); i++){
+        const location = freq.locations[i + 1 + index]
+        const directions = [{x: location.x - loc.x, y: location.y - loc.y}, {x: loc.x - location.x, y: loc.y - location.y}]
+        if (!uniqueLocs[loc.y]?.[loc.x]){
+          uniqueLocs[loc.y] = {...uniqueLocs[loc.y], [loc.x]: true}
+        }
+        if (!uniqueLocs[location.y]?.[location.x]){
+          uniqueLocs[location.y] = {...uniqueLocs[location.y], [location.x]: true}
+        }
+
+        directions.forEach(dir => {
+          const {x: stepX, y: stepY} = dir
+          let startPos = structuredClone(loc)
+          while(true){
+            const x = startPos.x + stepX
+            const y = startPos.y + stepY
+            const isValid = x < mapWidth && x >= 0 && y < mapLength && y >= 0
+            if (!isValid) {
+              break;
+            }
+            if (!uniqueLocs[y]?.[x]){
+              uniqueLocs[y] = {...uniqueLocs[y], [x]: true}
+            }
+            startPos = {x, y}
+          }
+        })
+
+      }
+    })
+  })
+
+  Object.values(uniqueLocs).forEach(loc => {
+    sum += Object.keys(loc).length
+  })
+
+  return sum;
 }
 
 await runSolution(day8b);
