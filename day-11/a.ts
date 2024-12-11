03a5c96ba001b6098bb078a6a2da29779c86e135
Floris <florisjoepie@gmail.com>
2024-12-11 11:10:02 +0100

    feat: day 11

--- day-11/a.ts
+++ day-11/a.ts
@@ -2,8 +2,33 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day11a(data: string[]) {
-  console.log(data);
-  return 0;
+  const stones = data[0].split(' ')
+
+  let oldArr = structuredClone(stones)
+  let newArr = []
+
+  const steps = 25
+
+  for(let i = 0; i < steps; i++){
+    oldArr.forEach(stone => {
+      const num = Number(stone)
+      if (num === 0){
+        newArr.push(String(1))
+        return
+      } else if (stone.length % 2 === 0){
+       const removeLeadingZeroes =  stone.substring(stone.length / 2).replace(/^0+/, '')
+        newArr.push(stone.substring(0, stone.length / 2), removeLeadingZeroes.length === 0 ? '0' : removeLeadingZeroes)
+        return
+      } else {
+        newArr.push(String(BigInt(String(num)) * BigInt(2024)))
+      } 
+    })
+
+    oldArr = structuredClone(newArr)
+    newArr = []
+  }
+
+  return oldArr.length;
 }
 
 await runSolution(day11a);
--- day-11/b.ts
+++ day-11/b.ts
@@ -2,8 +2,51 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day11b(data: string[]) {
-  console.log(data);
-  return 0;
+  const stones = data[0].split(' ')
+  const steps = 75
+
+  const map: Record<number, Record<string, number>> = {}
+
+
+  const returnStoneAmount = (stone: string, step: number): number => {
+    if (!map[step]){
+      map[step] = {}
+    }
+    if (map[step][stone]){
+      return map[step][stone]
+    }
+    if (step === steps){
+      return 1
+    }
+    const num = Number(stone)
+    if (num === 0){
+      const val = returnStoneAmount(String(1), step + 1)
+
+      map[step][stone] = val
+
+      return val
+    } else if (stone.length % 2 === 0){
+      const removeLeadingZeroes = stone.substring(stone.length / 2).replace(/^0+/, '')
+      const val = returnStoneAmount(stone.substring(0, stone.length / 2), step + 1) + returnStoneAmount(removeLeadingZeroes.length === 0 ? '0' : removeLeadingZeroes, step + 1)
+
+      map[step][stone] = val
+
+      return val
+    } else {
+      const val = returnStoneAmount(String(num * 2024), step + 1)
+      map[step][stone] = val
+
+      return val
+    } 
+  }
+
+  let sum = 0
+    stones.forEach(stone => {
+     sum += returnStoneAmount(stone, 0)
+    })
+
+
+  return sum;
 }
 
 await runSolution(day11b);
