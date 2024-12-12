05a7e7df316fadcee12ef851bd9003f3e3eb241e
Floris Jetses <floris.jetses@deptagency.com>
2024-12-12 20:36:48 +0100

    feat: day 12 b

--- day-12/b.ts
+++ day-12/b.ts
@@ -10,10 +10,17 @@ const directions: Record<string, [X, Y]> = {
   w: [-1, 0],
 }
 
+const diagonals: Record<string, [X, Y]> = {
+  ne: [1, -1],
+  se: [1, 1],
+  sw: [-1, 1],
+  nw: [-1, -1],
+}
+
 /** provide your solution as the return of this function */
 export async function day12b(data: string[]) {
   const map = toMatrix(data)
-  
+
   let sum = 0
   const locationsTraveled: string[] = []
 
@@ -21,16 +28,19 @@ export async function day12b(data: string[]) {
     const location: [number, number] = [x, y]
     if (locationsTraveled.includes(location.join('-'))) return
     let regionSize = 0;
-    let perimeter = 0;
     let corners = 0;
- 
+
     const findNextNode = (startPos: [number, number]) => {
-      let sides = 4;
+      let sides = 0;
       regionSize++
       locationsTraveled.push(startPos.join('-'))
       const [locX, locY] = startPos
+      let left = false
+      let right = false
+      let up = false
+      let down = false
 
-      Object.values(directions).forEach(dir => {
+      Object.values(directions).forEach((dir, index) => {
         const [dirX, dirY] = dir
         const newX = locX + dirX
         const newY = locY + dirY
@@ -38,18 +48,82 @@ export async function day12b(data: string[]) {
         const sideVal = map[newY]?.[newX]
 
         if (sideVal === plant){
-          sides--
+          sides++
+          if (index === 0) up = true
+          if (index === 1) right = true
+          if (index === 2) down = true
+          if (index === 3) left = true
           if (!locationsTraveled.includes(newLocation)){
             findNextNode([newX, newY])
           }
-        } 
+        }
       })
-      if (sides <= 2 && sides > 0){
+      const vertical = up && down
+      const horizontal = left && right
+      const plus = vertical && horizontal
+      const corner = (horizontal && up || horizontal && down || vertical && left || vertical && right)
+      const T = corner && sides === 3
+      const L = !horizontal && !vertical && sides === 2
+
+      if (sides === 0){
+        corners += 4
+      } else if (sides === 1){
         corners +=2
+      } else if (L) {
+        corners++
+        Object.values(diagonals).forEach((dir, index) => {
+          const [dirX, dirY] = dir
+          const newX = locX + dirX
+          const newY = locY + dirY
+          const sideVal = map[newY]?.[newX]
+          if (sideVal !== plant){
+            if (up && right && index === 0) {
+              corners++
+            }
+            if (right && down && index === 1) {
+              corners++
+            }
+            if (down && left && index === 2) {
+              corners++
+            }
+            if (left && up && index === 3) {
+              corners++
+            }
+          }
+        })
+      } else if (T) {
+        Object.values(diagonals).forEach((dir, index) => {
+          const [dirX, dirY] = dir
+          const newX = locX + dirX
+          const newY = locY + dirY
+          const sideVal = map[newY]?.[newX]
+          if (sideVal !== plant){
+            if (horizontal && up && index === 0){ corners++; console.log('up', index)}
+            if (horizontal && up && index === 3) {corners++; console.log('up', index)}
+
+            if (right && vertical && index === 1) {corners++; console.log('right', index)}
+            if (right && vertical && index === 0) {corners++; console.log('right', index)}
+
+            if (down && horizontal && index === 1) {corners++; console.log('down', index)}
+            if (down && horizontal && index === 2){ corners++; console.log('down', index)}
+
+            if (left && vertical && index === 2) {corners++; console.log('left', index)}
+            if (left && vertical && index === 3){ corners++; console.log('left', index)}
+          }
+        })
+      } else if (plus) {
+        Object.values(diagonals).forEach((dir) => {
+          const [dirX, dirY] = dir
+          const newX = locX + dirX
+          const newY = locY + dirY
+          const sideVal = map[newY]?.[newX]
+          if (sideVal !== plant){
+            corners++
+          }
+        })
       }
     }
     findNextNode(location)
-    console.log(plant, corners)
     sum += (regionSize * corners)
   })
   return sum;
