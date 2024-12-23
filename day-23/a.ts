9a061ddcf4e5c8382618447ee6a8ea30fd33d2e1
Floris <florisjoepie@gmail.com>
2024-12-24 00:41:41 +0100

    feat: day 23

--- day-23/a.ts
+++ day-23/a.ts
@@ -2,8 +2,38 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day23a(data: string[]) {
-  console.log(data);
-  return 0;
+  const stored: Record<string, Record<string, boolean>> = {}
+  data.forEach(row => {
+    const [val1, val2] = row.replace('\r', '').toLowerCase().split('-')
+
+    stored[val1] = {...stored[val1], [val2]: true}
+    stored[val2] = {...stored[val2], [val1]: true}
+  })
+
+  const interConnected = new Set<string>()
+
+  data.forEach(row => {
+    const [val1, val2] = row.replace('\r', '').toLowerCase().split('-')
+    const connected1 = stored[val1]
+    const connected2 = stored[val2]
+    const filtered = Object.keys(connected1).filter(x => Object.keys(connected2).some(y => x === y && x !== val2 && y !== val1)
+)
+    filtered.forEach(val => {
+      interConnected.add([val1, val2, val].sort().join())
+    })
+  })
+
+  let count = 0;
+  interConnected.forEach(key => {
+    const test = key.split(',')
+    for (const item of test){
+      if (item.startsWith('t')){
+        count++
+        break;
+      }
+    }
+  })
+  return count;
 }
 
 await runSolution(day23a);
--- day-23/b.ts
+++ day-23/b.ts
@@ -2,8 +2,48 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day23b(data: string[]) {
-  console.log(data);
-  return 0;
+  const stored: Record<string, Record<string, boolean>> = {}
+
+  data.forEach(row => {
+    const [val1, val2] = row.replace('\r', '').toLowerCase().split('-')
+
+    stored[val1] = {...stored[val1], [val2]: true}
+    stored[val2] = {...stored[val2], [val1]: true}
+  })
+
+  const cliques: Set<string>[] = []
+
+  const getVertices = (candidates: Set<string>, edges: string[]) => {
+    const newSet = new Set<string>()
+    edges.forEach(e => {
+      if (candidates.has(e)){
+        newSet.add(e)
+      }
+    })
+
+    return newSet
+  }
+ 
+
+  const BronKerbosch = (clique: Set<string>, candidates: Set<string>, visited: Set<string>) => {
+    if (!candidates.size && !visited.size){
+      cliques.push(clique)
+      return
+    }
+    candidates.forEach(candidate => {
+      BronKerbosch(new Set(clique).add(candidate), getVertices(candidates, Object.keys(stored[candidate])), getVertices(visited, Object.keys(stored[candidate])))
+      candidates.delete(candidate)
+      visited.add(candidate)
+    })
+    
+  }
+
+  BronKerbosch(new Set(), new Set(Object.keys(stored)), new Set())
+
+  const biggest = cliques.sort((a, b) => a.size - b.size).at(-1)
+
+  const test = Array.from(biggest).sort().join()
+  return test;
 }
 
 await runSolution(day23b);
